c4d4727a50ea70694bfb8133f124f1dd99572c46
mayur limbekar <mayurlimbekar7@gmail.com>
2025-08-20 23:00:11 +0530

    Initial set up done

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Mayur Limbekar on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
